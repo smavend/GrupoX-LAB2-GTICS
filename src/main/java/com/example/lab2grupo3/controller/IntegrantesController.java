d067bceac1dc681c3429c7a9066e42b600bfbfe2
amanr <amanrique068@gmail.com>
2023-04-04 22:04:07 -0500

    ultimo

--- src/main/java/com/example/lab2grupo3/controller/IntegrantesController.java
+++ src/main/java/com/example/lab2grupo3/controller/IntegrantesController.java
@@ -1,13 +1,16 @@
 package com.example.lab2grupo3.controller;
 
 import com.example.lab2grupo3.entity.Integrante;
+import com.example.lab2grupo3.entity.Proveedor;
 import com.example.lab2grupo3.repository.IntegranteRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/integrante")
@@ -21,5 +24,14 @@ public class IntegrantesController {
         List<Integrante> integranteList = integranteRepository.findAll();
         model.addAttribute("integranteList", integranteList);
         return "integrante";
+    }
+    @GetMapping("/integrante/borrar")
+    public String borrar(@RequestParam("id") int id){
+        Optional<Integrante> optProv =integranteRepository.findById(id);
+        if(optProv.isPresent()){
+            integranteRepository.deleteById(id);
+        }
+        return "redirect:/integrante";
+
     }
 }
