82f032add6cef5af6e2c8a996fd18bf922a5221e
amanr <amanrique068@gmail.com>
2023-04-04 21:05:46 -0500

    modelo

--- src/main/java/com/example/lab2grupo3/controller/ProveedoresController.java
+++ src/main/java/com/example/lab2grupo3/controller/ProveedoresController.java
@@ -1,4 +1,12 @@
 package com.example.lab2grupo3.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProveedoresController {
+    @GetMapping("/proveedor")
+    public String lista(){
+        return "proveedor";
+    }
 }
--- src/main/java/com/example/lab2grupo3/entity/Proveedor.java
+++ src/main/java/com/example/lab2grupo3/entity/Proveedor.java
@@ -1,4 +1,25 @@
 package com.example.lab2grupo3.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table{name="proveedores"}
 public class Proveedor {
+    @Id
+    @Column(nullable = false)
+    private int idProveedores;
+    @Column(name = "Nombre")
+    private String nombre;
+    @Column(name = "Telefono")
+    private int telefono;
+    @Column(name = "Elemento")
+    private String elemento;
 }
