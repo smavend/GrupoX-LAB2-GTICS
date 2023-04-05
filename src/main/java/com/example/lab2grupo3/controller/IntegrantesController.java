67fed3f8feaeec9da6b4f7929fba4ba75f08beff
noe <noe.jaraq@gmail.com>
2023-04-04 21:54:48 -0500

    integrante lista

--- src/main/java/com/example/lab2grupo3/controller/IntegrantesController.java
+++ src/main/java/com/example/lab2grupo3/controller/IntegrantesController.java
@@ -1,4 +1,25 @@
 package com.example.lab2grupo3.controller;
 
+import com.example.lab2grupo3.entity.Integrante;
+import com.example.lab2grupo3.repository.IntegranteRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/integrante")
 public class IntegrantesController {
+    final IntegranteRepository integranteRepository;
+    public IntegrantesController(IntegranteRepository integranteRepository){
+        this.integranteRepository = integranteRepository;
+    }
+    @GetMapping(value = {"", "/"})
+    public String index(Model model){
+        List<Integrante> integranteList = integranteRepository.findAll();
+        model.addAttribute("integranteList", integranteList);
+        return "integrante";
+    }
 }
--- src/main/java/com/example/lab2grupo3/repository/IntegranteRepository.java
+++ src/main/java/com/example/lab2grupo3/repository/IntegranteRepository.java
@@ -1,4 +1,9 @@
 package com.example.lab2grupo3.repository;
 
-public interface IntegranteRepository {
+import com.example.lab2grupo3.entity.Integrante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IntegranteRepository extends JpaRepository<Integrante, Integer> {
 }
